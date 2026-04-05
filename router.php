1039442f1cc448a6f003035a958750658e2498ce
Ahmed Kamel <ahmedkamel180998@gmail.com>
2026-04-05 22:36:43 +0000

    Separate Router files to handle routing logic and keep index.php clean

---
+++ router.php
@@ -0,0 +1,12 @@
+<?php
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$routes = require basePath('routes.php');
+
+if (array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    http_response_code(404);
+    require basePath($routes['404']);
+}
