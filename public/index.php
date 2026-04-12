91b7d0b5a43e7617c8e69f9842215787ad27fe49
Ahmed Kamel <ahmedkamel180998@gmail.com>
2026-04-12 15:56:02 +0000

    Add Composer with PSR-4 autoloading
    Add composer.json with PSR-4 autoload for Framework\ and App\ namespaces
    Add composer.lock
    Remove spl_autoload_register from public/index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,9 @@
 <?php
 require '../helpers.php';
-spl_autoload_register(function ($class) {
-    $path = basePath("Framework/{$class}.php");
-    if (file_exists($path)) {
-        require $path;
-    }
-});
-
-// require basePath('Framework/Database.php');
 $dbConfig = require basePath('config/db.php');
 $db = new Database($dbConfig);
-// require basePath('Framework/Router.php');
-// Instantiate the router
 $router = new Router();
+
 // Load the routes
 $routes = require basePath('routes.php');
 // Get the current request URI and method
