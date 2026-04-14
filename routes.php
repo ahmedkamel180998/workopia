e64b4684530bef9b6f604483f11283dd51bc5801
Ahmed Kamel <ahmedkamel180998@gmail.com>
2026-04-14 00:48:31 +0000

    Add user authentication routes and views for login and registration

--- routes.php
+++ routes.php
@@ -7,3 +7,7 @@ $router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
+
+// Auth routes
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
